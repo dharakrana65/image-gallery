178af7c7a888fae64a43f763542bd2d8618d1ec9
dharu <dharakrana65@gmail.com>
2023-10-29 22:51:43 +0530

    new code

--- src/ApiUrl.jsx
+++ src/ApiUrl.jsx
@@ -1,18 +1,22 @@
-import axios from "axios"
+import axios from "axios";
 
+// Create an instance of the Axios HTTP client with specific configuration.
 const API = axios.create({
-    baseURL: "https://pixabay.com/api/",
-    headers : {
-        "Accept" : "application/json",
-        "Content-Type" : "application/json"
-    },
-    params : {
-        // key : process.env.REACT_PIXABAY_API_KEY,
-        key : '40343383-4489e18d267dd59be7f34c713',
-        image_type : "all",
-        orientation : "vertical",
-        min_height : 5
-    },
-})
+  baseURL: "https://pixabay.com/api/",
+  headers: {
+    // Define request headers 
+    Accept: "application/json", // Specify that we accept JSON responses.
+    "Content-Type": "application/json", // Specify that we're sending JSON data in requests.
+  },
+  params: {
+    // Define default parameters that will be sent with every request.
+    key: "40343383-4489e18d267dd59be7f34c713", // API key for authentication.
+    image_type: "all", 
+    orientation: "vertical", 
+    safeSearch: true, 
+    // category: "food",    //Accepted values: backgrounds, fashion, nature, science, education, feelings, health, people, religion, places, animals, industry, computer, food, sports, transportation, travel, buildings, business, music
+   
+  },
+});
 
 export default API;
--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,76 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import API from "./ApiUrl.jsx";
 import ImgContainer from "./pages/ImgContainer.jsx";
 import "./index.css";
+import debounce from "lodash.debounce";
+
 const App = () => {
-  let pageNum = 1;
-  const [images, setImages] = useState([]);
-  const [totalPages, setTotalPages] = useState(true);
-  const [searchQuery, setSearchQuery] = useState("");
-  const fetchImages = (pageNumber) => {
-    API.get("/", { params: { page: pageNumber, q: searchQuery } })
+  const [images, setImages] = useState([]); // storing images
+  const [totalPages, setTotalPages] = useState(true); // total pages of images
+  const [searchQuery, setSearchQuery] = useState(""); // the search query
+  const [loading, setLoading] = useState(false);
+  let pageNum = 1; 
+
+  // Debounced function to fetch images with a delay after user input
+  const debouncedFetchImages = debounce((pageNumber, query) => {
+    setLoading(true); // Set loading state to true
+    API.get("/", { params: { page: pageNumber, q: query } })
       .then((response) => {
-          console.log(response.data);
-          setImages([...images, response.data.hits]);
-          setTotalPages(response.data.totalHits / response.data.hits.length);
-          console.log("image array :", images);
-          console.log(totalPages);
+        setLoading(false); // Set loading state to false
+        const imagesForCurrentPage = response.data.hits; // Images for the current page
+        setImages((prevImages) => [...prevImages, imagesForCurrentPage]);
+        setTotalPages(response.data.totalHits / response.data.hits.length);
       })
-      .catch((err) => console.error(err));
-  };
+      .catch((err) => {
+        setLoading(false); // Set loading state to false on error
+        console.error(err);
+      });
+  }, 200);
 
   useEffect(() => {
+    // fetch images when the component mounts
+    debouncedFetchImages(pageNum, searchQuery);
+  }, []);
+
+  //fetch images when the component or search query updates
+  useEffect(() => {
     setImages([]);
-    fetchImages(pageNum);
+    setTotalPages(true);
+    debouncedFetchImages(pageNum, searchQuery); // Fetch images
   }, [searchQuery]);
+
   return (
     <>
-      <div className="main-wrapper">
-        <div className="search-bar">
-          <div className="heading-container">
-            <span>Immerse in Imagery</span>
-            <span>Where Every Picture Tells a Tale</span>
-          </div>
-          <input
-            type="text"
-            placeholder="Search images"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
+      <div className="search-bar">
+        <div className="heading-container">
+          <span>Immerse in Imagery</span>
+          <span>Where Every Picture Tells a Tale</span>
         </div>
-        {images.map((data, index) => (
-          <ImgContainer image={data} key={index}></ImgContainer>
-        ))}
+        <input
+          type="text"
+          placeholder="Search images"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </div>
+
+      <div className="main-wrapper">
+        {Array.isArray(images) && images.length > 0 ? (
+          images.map((data, index) => (
+            <ImgContainer image={data} key={index}></ImgContainer>
+          ))
+        ) : loading ? (
+          <p>Loading images...</p>
+        ) : (
+          <p>No images to display.</p>
+        )}
       </div>
 
       {totalPages > 1 && (
-        <button className="load-btn" onClick={() => fetchImages(pageNum + 1)}>
+        <button
+          className="load-btn"
+          onClick={() => debouncedFetchImages(pageNum + 1, searchQuery)}
+        >
           Load More
         </button>
       )}
